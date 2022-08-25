58ee5223fb711377a69886a97a1e08054532885a
Hansanca <hansanca@gmail.com>
2022-08-25 18:15:44 -0400

    test3

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@
  */
 
 module.exports = {
-  testTimeout: 10000,
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -136,7 +135,7 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['./jest.setup.js'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
